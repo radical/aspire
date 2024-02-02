4eb5fb80b34d91cff2568162d311a93aec98be47
Ankit Jain <radical@gmail.com>
2024-02-01 20:51:32 -0500

    wip

---
+++ tests/Aspire.EndToEnd.Tests/Common/End2EndTestBase.cs
@@ -0,0 +1,116 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics.CodeAnalysis;
+using Xunit.Abstractions;
+using System.Text;
+using System.Globalization;
+
+namespace Aspire.End2End.Tests;
+
+public class End2EndTestBase
+{
+    public const string DefaultTargetFramework = "net8.0";
+    private const string NuGetInsertionTag = "<!-- TEST_RESTORE_SOURCES_INSERTION_LINE -->";
+
+    public static readonly BuildEnvironment s_buildEnv = new();
+    public static string GetNuGetConfigPathFor(string targetFramework)
+        => Path.Combine(BuildEnvironment.TestDataPath, targetFramework == "net9.0" ? "nuget9.config" : "nuget8.config");
+
+    public ITestOutputHelper TestOutput { get; init; }
+    protected string? _projectDir;
+    protected string? _logPath;
+    protected string? _nugetPackagesDir;
+
+    public End2EndTestBase(ITestOutputHelper testOutput)
+    {
+        TestOutput = new TestOutputWrapper(testOutput);
+    }
+
+    [MemberNotNull(nameof(_projectDir), nameof(_logPath))]
+    protected void InitPaths(string id)
+    {
+        if (_projectDir == null)
+        {
+            _projectDir = Path.Combine(BuildEnvironment.TmpPath, id);
+        }
+        _logPath = Path.Combine(s_buildEnv.LogRootPath, id);
+        _nugetPackagesDir = Path.Combine(BuildEnvironment.TmpPath, "nuget", id);
+
+        if (Directory.Exists(_nugetPackagesDir))
+        {
+            Directory.Delete(_nugetPackagesDir, recursive: true);
+        }
+
+        Directory.CreateDirectory(_nugetPackagesDir!);
+        Directory.CreateDirectory(_logPath);
+    }
+
+    protected void InitProjectDir(string dir, bool addNuGetSourceForLocalPackages = true, string targetFramework = DefaultTargetFramework)
+    {
+        Directory.CreateDirectory(dir);
+        File.WriteAllText(Path.Combine(dir, "Directory.Build.props"), "<Project><PropertyGroup><ManagePackageVersionsCentrally>false</ManagePackageVersionsCentrally></PropertyGroup></Project>");//s_buildEnv.DirectoryBuildPropsContents);
+        File.WriteAllText(Path.Combine(dir, "Directory.Build.targets"), "<Project/>");//s_buildEnv.DirectoryBuildTargetsContents);
+        File.WriteAllText(Path.Combine(dir, "Directory.Packages.props"), "<Project/>");//s_buildEnv.DirectoryBuildPropsContents);
+
+        string targetNuGetConfigPath = Path.Combine(dir, "nuget.config");
+        if (addNuGetSourceForLocalPackages)
+        {
+            File.WriteAllText(targetNuGetConfigPath,
+                                GetNuGetConfigWithLocalPackagesPath(
+                                            GetNuGetConfigPathFor(targetFramework),
+                                            s_buildEnv.BuiltNuGetsPath));
+        }
+        else
+        {
+            File.Copy(GetNuGetConfigPathFor(targetFramework), targetNuGetConfigPath);
+        }
+    }
+
+    protected static string GetNuGetConfigWithLocalPackagesPath(string templatePath, string localNuGetsPath)
+    {
+        string contents = File.ReadAllText(templatePath);
+        if (contents.IndexOf(NuGetInsertionTag, StringComparison.InvariantCultureIgnoreCase) < 0)
+        {
+            throw new ArgumentException($"Could not find {NuGetInsertionTag} in {templatePath}");
+        }
+
+        return contents.Replace(NuGetInsertionTag, $@"<add key=""nuget-local"" value=""{localNuGetsPath}"" />");
+    }
+
+    public static string GetRandomId() => FixupSymbolName(Path.GetRandomFileName());
+
+    // FIXME: move to a TestUtils class
+    private static readonly char[] s_charsToReplace = new[] { '.', '-', '+' };
+    public static string FixupSymbolName(string name)
+    {
+        UTF8Encoding utf8 = new();
+        byte[] bytes = utf8.GetBytes(name);
+        StringBuilder sb = new();
+
+        foreach (byte b in bytes)
+        {
+            if ((b >= (byte)'0' && b <= (byte)'9') ||
+                    (b >= (byte)'a' && b <= (byte)'z') ||
+                    (b >= (byte)'A' && b <= (byte)'Z') ||
+                    (b == (byte)'_'))
+            {
+                sb.Append((char)b);
+            }
+            else if (s_charsToReplace.Contains((char)b))
+            {
+                sb.Append('_');
+            }
+            else
+            {
+                sb.Append(CultureInfo.InvariantCulture, $"_{b:X}_");
+            }
+        }
+        if (Char.IsDigit(sb[0]) || s_charsToReplace.Contains(sb[0]))
+        {
+            sb[0] = '_';
+        }
+
+        return sb.ToString();
+    }
+}
